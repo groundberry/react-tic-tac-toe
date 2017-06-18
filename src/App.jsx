7c7ae5168e49d496b47bd682fc27b4de58c06815
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2017-06-18 15:54:10 -0700

    Lint all the things

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Tit Tac Toe</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
